473fccb4c5649b3b3cbda07c5f9595ea45754104
Mansibansal9648 <mansibansal12345@gmail.com>
2023-04-15 12:13:35 +0530

    basic form design

---
+++ client/src/components/Home.js
@@ -0,0 +1,59 @@
+export const Home=()=>{
+    return(
+        <div>
+            <h4>Please fill the below Employee Details</h4>
+            Name = 
+            <input type="text">
+            </input>
+            <br></br>
+            <br></br>
+            Role = 
+            <input type= "text">
+            </input>
+            <br></br>
+            <br></br>
+            Designation = 
+            <input type= "text">
+            </input>
+            <br></br>
+            <br></br>
+            Project = 
+            <input type="text">
+            </input>
+            <br></br>
+            <br></br>
+            Experience = 
+            <input type="text">
+            </input>
+            <br></br>
+            <br></br>
+            Email Id = 
+            <input type="text">
+            </input>
+            <br></br>
+            <br></br>
+            Salary = 
+            <input type = "text">
+            </input>
+            <br></br>
+            <br></br>
+            Phone No. = 
+            <input type="text">
+            </input>
+            <br></br>
+            <br></br>
+            Joining Date = 
+            <input type="text">
+            </input>
+            <br></br>
+            <br></br>
+            Previous Company = 
+            <input type = "text">
+            </input>
+            <br></br>
+            <br></br>
+            <input type ="button" value="Submit">
+            </input>
+        </div>
+    )
+}
