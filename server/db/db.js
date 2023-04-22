ab815219d58a855e0641e7fe1d1aa03caa707b43
Sooraj Yadav <frankSoorj@gmail.com>
2023-04-22 14:49:20 +0530

    db connection error handling using event driven model

--- server/db/db.js
+++ server/db/db.js
@@ -1,21 +1,20 @@
 const mongoose = require("mongoose");
 const url =
   "mongodb+srv://MonaDev:Mona123@cluster0.01rixba.mongodb.net/employee_mgmt_system?retryWrites=true&w=majority";
+// "mongodb://localhost:27017/employee_data";
 const database = async () => {
-  try {
-    await mongoose.connect(
-      url,
-      // {
-      //  useNewUrlParser:true,
-      //  useCreateIndex:true,
-      // useUnifiedTopology:true,
-      // useFindAndModify:false},
-      () => {
-        console.log("Connected to database successfully");
-      }
-    );
-  } catch (e) {
-    console.log("Can not connect to database");
-  }
+  mongoose.connect(url, {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false,
+  });
+
+  // By using event driven model
+  const db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error: "));
+  db.once("open", function () {
+    console.log("Connected successfully");
+  });
 };
-module.exports = { database, url};
+module.exports = { database, url };
